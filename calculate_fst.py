cbbbae1425c16c84843fe4172a6fc602b676d5d6
Meera Chotai <62544870+meerachotai@users.noreply.github.com>
2021-07-01 13:56:04 +0530

    F_st vs. distance update + output table

--- calculate_fst.py
+++ calculate_fst.py
@@ -1,24 +1,30 @@
-import pandas as pd
-import io
+# python calculate_fst.py test.vcf fst.png table.txt 5 5 10 1000 0.005
 import math
+import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
-input_vcf = str(sys.argv[1])
+file = str(sys.argv[1])
 new_file = str(sys.argv[2])
-numPopEW = int(sys.argv[3]) 
-numPopNS = int(sys.argv[4]) 
-sampleSize = int(sys.argv[5])
+table = str(sys.argv[3])
+numPopEW = int(sys.argv[4]) 
+numPopNS = int(sys.argv[5]) 
+sampleSize = int(sys.argv[6])
+ne = int(sys.argv[7])
+mig = float(sys.argv[8])
 
 populations = numPopEW * numPopNS
 
 line = ""
 index = 0
-all_fst = []
-all_pos = []
- 
+
+sumFstMatrix = np.zeros((populations, populations))
+snpMatrix = np.zeros((populations, populations))
+sqFstMatrix = np.zeros((populations, populations))
+
 first_line = True
-with open(input_vcf, 'r') as f:
+
+with open(file, 'r') as f:
     for line in f:
         if line.startswith("##"):
             continue
@@ -26,46 +32,117 @@ with open(input_vcf, 'r') as f:
             if(first_line):
                 line = line.strip().split("\t")
                 for num,string in enumerate(line):
-                    if(any(char.isdigit() for char in string)):
-                        index = num
+                    if(any(char.isdigit() for char in string)): # looking for index of first sample
+                        index = num 
                         break
                 first_line = False
                 continue
             else:
-                line = line.strip().split("\t")
-                pos = line[1]
+                line = line.strip().split("\t") # each line is kth SNP
                 line_sub = "".join(line[index:]).replace("|", "")
-                top = 0
-                h = 0
-                overall_p = 0
+
+                # initialize a set for every SNP k
+                hsMatrix = np.zeros((populations, populations))
+                htMatrix = np.zeros((populations, populations))
+                fstMatrix = np.zeros((populations, populations))
+                
                 for i in range(populations):
-                    bottom = top + (sampleSize *2)  # for diploid, x2
-                    p = line_sub[top:bottom].count("1") # count freq of alternate allele
-                    top = bottom
+                    top_i = (i * sampleSize * 2) # start index for population i 
+                    bottom_i = top_i + (sampleSize *2) # end index for population i
                     
-                    overall_p += p # incrementing alt allele freq for total population
-                    p = p / (sampleSize * 2) # p / (2 * subpop size) 
-                    h += (2 * p * (1-p) * sampleSize) # 2pq * N_i, weighted by subpop size
+                    p_1 = line_sub[top_i:bottom_i].count("1") # count freq of alternate allele in ith population
+                    p_1_frac = p_1 / (sampleSize * 2) # as a fraction of the diploid subpop
+                    
+                    for j in range(i+1,populations):
+                        
+                        top_j = (j * sampleSize * 2)
+                        bottom_j = top_j + (sampleSize *2)
+                        p_2 = line_sub[top_j:bottom_j].count("1")
+                        
+                        if p_1 != p_2: # consider ONLY if they are segregating
+                            snpMatrix[i,j] += 1
+                        else:
+                            continue
+                            
+                        p_2_frac = p_2 / (sampleSize * 2)
+                        
+                        # H_s = 2*p_i*q_i*N_i + 2*p_j*q_j*N_j / (N_i + N_j)
+                        h = (2 * p_1_frac * (1-p_1_frac) * sampleSize) + (2 * p_2_frac * (1-p_2_frac) * sampleSize) 
+                        sub_h = h / (2 * sampleSize)
+                        
+                        overall_p = p_1 + p_2 # p for total population
+                        overall_p = overall_p / (2* sampleSize * 2) # overall_p / (2 * total_pop)
+                        total_h = overall_p * (1 - overall_p) * 2 # 2pq for total population
+                        
+                        hsMatrix[i,j] = sub_h
+                        htMatrix[i,j] = total_h
+                        
+                        if total_h == 0:
+                            continue
+                        fstMatrix[i,j] = (total_h - sub_h) / total_h
+                            
+                sqFst = np.multiply(fstMatrix, fstMatrix) # (F_st)^2
                 
-                overall_p = overall_p / (2* sampleSize * populations) # p / (2 * total_pop)
+                sqFstMatrix = np.add(sqFstMatrix, sqFst) # sum((F_st)^2)
+                sumFstMatrix = np.add(sumFstMatrix, fstMatrix) # sum(F_st)
                 
-                sub_h = h / (populations * sampleSize) # h / total_pop, weighted avg. of h
-                total_h = overall_p * (1 - overall_p) * 2 # 2pq for total population
-                fst = (total_h - sub_h) / total_h
-                
-                all_fst.append(fst)
-                all_pos.append(pos)
+meanFstMatrix = np.divide(sumFstMatrix, snpMatrix, out=np.zeros((populations, populations)), where=snpMatrix!=0)
 
-plt.plot(all_pos, all_fst)
-plt.ylabel('F_st')
-plt.xlabel('SNP position')
-plt.xticks([])
+# (E[x])^2 - square the meanFstMatrix
+second_half = np.multiply(meanFstMatrix,meanFstMatrix)
 
+# E[x^2] - take the mean of sqFstMatrix
+first_half = np.divide(sqFstMatrix, snpMatrix, out=np.zeros((populations, populations)), where=snpMatrix!=0)
+
+# variance = E[x^2] - (E[x])^2
+variance = np.subtract(first_half, second_half)
+variance = np.absolute(variance)
+
+# standard deviation
+sd = np.sqrt(variance)
+# standard error = sd / square-root(snpMatrix)
+sqrtSNPmatrix = np.sqrt(snpMatrix)
+seFstMatrix = np.divide(sd, sqrtSNPmatrix, out=np.zeros((populations, populations)), where=sqrtSNPmatrix!=0)
+
+#------------------------ calculate by distance ------------------------
+
+meanSEdist = {}
+for i in range(populations):
+    for j in range(i+1,populations):
+        x_i = math.floor(i/numPopEW) # goes row-by-row
+        y_i = i % numPopEW
+        x_j = math.floor(j/numPopEW)
+        y_j = j % numPopEW
+        distance = math.sqrt(((x_i - x_j) ** 2) + ((y_i - y_j) ** 2)) # calculate distance
+        if distance not in meanSEdist:
+            meanSEdist[distance] = [[],[]]
+        meanSEdist[distance][0].append(meanFstMatrix[i][j])
+        meanSEdist[distance][1].append(seFstMatrix[i][j])
+
+x = [] # distance
+y = [] # mean 
+e = [] # SE
+for key in meanSEdist:
+    value = meanSEdist[key]
+    for i in range(len(value[0])):
+        x.append(key)
+        y.append(value[0][i])
+        e.append(value[1][i])
+    
+title = "$N_e$: " + str(ne) + ", Sample size: " + str(sampleSize) + ", Homogenous Migration Rate: " + str(mig)
+
+plt.scatter(x, y)
+plt.errorbar(x, y, yerr = e, fmt="o")
+plt.xlabel("distance between populations")
+plt.ylabel("mean $F_{ST}$")
+plt.suptitle("$F_{ST}$ vs. distance")
+plt.title(title, fontsize = 10)
+
+write_tab = open(table, "w")
+# write_tab.write("Distance\tMean_FST\tSE_FST\n")
+
+for i in range(len(x)):
+    tb = [str(x[i]), str(y[i]), str(e[i])]
+    write_tab.write("\t".join(tb) + "\n")
+write_tab.close()
 plt.savefig(new_file)
-                
-
-#                 print(sub_h)
-#                 print(total_h)
-#                 print("F_st =",fst * 100, "%")
-
-				

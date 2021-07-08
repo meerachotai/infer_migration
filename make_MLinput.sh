47dfffce2f5cb00858b694c0e82bd9b47918f67d
Meera Chotai <62544870+meerachotai@users.noreply.github.com>
2021-07-08 21:59:03 +0530

    Update make_MLinput.sh

--- make_MLinput.sh
+++ make_MLinput.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-# ./make_MLinput.sh $( pwd ) $( pwd ) $( pwd )/output_3 5 1000 10 3 0.001 0.005 0.01 0.015 0.02 0.025 0.03 0.035 0.04 0.05
+# ./make_MLinput.sh $( pwd ) $( pwd ) $( pwd )/output 5 1000 10 3 0.001 0.005 0.01 0.015 0.02 0.025 0.03 0.035 0.04 0.05
 # qsub -V -N job_ML -cwd -j y -o qsub_logs/ML.txt -m bae -b y -l h_rt=10:00:00,h_data=30G $cmd
-# cut -f 27 -d ',' output/EW.5_NS.5_N.1000_n.10_input.csv
 
 scripts_dir=$1
 shift # shift options to left
@@ -30,10 +29,10 @@ do
 
 	# run scripts to get output table files
 	echo Running allele_freq.py
-	${scripts_dir}/allele_freq.py ${scripts_dir}/EW.${size}_NS.${size}_mig.${i}_N.${Ne}_n.${sampleSize}_${seed}.vcf ${outdir}/EW.${size}_NS.${size}_mig.${i}_N.${Ne}_n.${sampleSize}_${seed}_freq.png ${outdir}/EW.${size}_NS.${size}_mig.${i}_N.${Ne}_n.${sampleSize}_${seed}_freq.txt $size $size $sampleSize $Ne $i
+	${scripts_dir}/allele_freq.py ${vcfDir}/EW.${size}_NS.${size}_mig.${i}_N.${Ne}_n.${sampleSize}_${seed}.vcf ${outdir}/EW.${size}_NS.${size}_mig.${i}_N.${Ne}_n.${sampleSize}_${seed}_freq.png ${outdir}/EW.${size}_NS.${size}_mig.${i}_N.${Ne}_n.${sampleSize}_${seed}_freq.txt $size $size $sampleSize $Ne $i
 	
-	echo Running calculate_fst.py
-	${scripts_dir}/calculate_fst.py ${scripts_dir}/EW.${size}_NS.${size}_mig.${i}_N.${Ne}_n.${sampleSize}_${seed}.vcf ${outdir}/EW.${size}_NS.${size}_mig.${i}_N.${Ne}_n.${sampleSize}_${seed}_fst.png ${outdir}/EW.${size}_NS.${size}_mig.${i}_N.${Ne}_n.${sampleSize}_${seed}_fst.txt $size $size $sampleSize $Ne $i
+	# echo Running calculate_fst.py
+	${scripts_dir}/calculate_fst.py ${vcfDir}/EW.${size}_NS.${size}_mig.${i}_N.${Ne}_n.${sampleSize}_${seed}.vcf ${outdir}/EW.${size}_NS.${size}_mig.${i}_N.${Ne}_n.${sampleSize}_${seed}_fst.png ${outdir}/EW.${size}_NS.${size}_mig.${i}_N.${Ne}_n.${sampleSize}_${seed}_fst.txt $size $size $sampleSize $Ne $i
 	
 	# declare output table files variables
 	freq="${outdir}/EW.${size}_NS.${size}_mig.${i}_N.${Ne}_n.${sampleSize}_${seed}_freq.txt"

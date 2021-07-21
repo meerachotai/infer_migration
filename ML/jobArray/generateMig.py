c97adfcbfab605de68295e79de5606744287e4d4
Meera Chotai <62544870+meerachotai@users.noreply.github.com>
2021-07-21 16:47:58 -0700

    Add files via upload

---
+++ ML/jobArray/generateMig.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+# generateMig.py 0.0001 0.01 1 5 100
+import random
+import sys
+
+lower = float(sys.argv[1])
+upper = float(sys.argv[2])
+asym = int(sys.argv[3]) # operates like boolean 0/1
+seed = float(sys.argv[4])
+n = int(sys.argv[5])
+
+# upper = 0.01
+# lower = 0.0001
+# asym = 0
+# seed = 5
+# n = 100
+
+random.seed(seed)
+
+file = open("mig_val.txt", "w")
+
+if(asym != 0): # three equal, one different
+    for i in range(0,n):
+        mig_sym = str(round(random.uniform(0.0001, 0.01),4))
+        mig_asym = str(round(random.uniform(0.0001, 0.01),4))
+        out_list = [mig_sym, mig_sym, mig_sym, mig_asym]
+        random.shuffle(out_list)
+        out = "\t".join(out_list)
+        file.write(out + "\n")
+else:
+    for i in range(0,n): # all equal
+        mig_sym = str(round(random.uniform(0.0001, 0.01),4))
+        out = "\t".join([mig_sym, mig_sym, mig_sym, mig_sym])
+        file.write(out + "\n")
+
+file.close()
---
+++ ML/jobArray/migJobArray.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#$ -cwd
+#$ -o qsub_logs/MLjobarray.txt
+#$ -j y
+#$ -l h_rt=5:00:00,h_data=30G
+#$ -m a
+#$ -V
+
+# refer https://www.hoffman2.idre.ucla.edu/Using-H2/Computing/Computing.html#running-array-jobs for more info on job arrays like this one
+
+# FIRST run: generateMig.py to get file "mig_val.txt"
+# for symmetric values: generateMig.py 0.0001 0.01 0 $seed $n
+# for asymmetric values: generateMig.py 0.0001 0.01 1 $seed $n
+
+# THEN run this file:
+# qsub -t 1-$n migJobArray.sh $( pwd ) $( pwd ) $( pwd )/output_jobarray 5 1000 10 5
+
+scripts_dir=$1
+vcfDir=$2
+outdir=$3
+size=$4
+Ne=$5
+sampleSize=$6
+seed=$7
+
+if [ -f "mig_val.txt" ]; then
+    line=`sed -n ${SGE_TASK_ID}p mig_val.txt`
+else
+	exit 1
+fi
+ 
+migEW=$(echo $line | cut -d " " -f 1)
+migWE=$(echo $line | cut -d " " -f 2)
+migNS=$(echo $line | cut -d " " -f 3)
+migSN=$(echo $line | cut -d " " -f 4)
+
+
+. /u/local/Modules/default/init/modules.sh
+
+module load python/3.6.1
+# export PATH="/u/home/m/mchotai/anaconda3/bin:$PATH"
+
+# run the simulation (around 3 hours)
+python steppingStoneSimulation.py $seed ${vcfDir}/EW.${size}_NS.${size}_migEW.${migEW}_migWE.${migWE}_migNS.${migNS}_migSN.${migSN}_N.${Ne}_n.${sampleSize}_${seed}.vcf $size $size ${Ne} ${sampleSize} $migEW $migWE $migNS $migSN
+# add a line to the input file (around 1-2 hours)
+/bin/bash make_MLinput_asymmetric.sh $scripts_dir $vcfDir $outdir $size $Ne $sampleSize $seed $migEW $migWE $migNS $migSN

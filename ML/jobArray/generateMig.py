7a21dffb50a32d3d99c159c0fd546472594b3ae7
Meera Chotai <62544870+meerachotai@users.noreply.github.com>
2021-07-24 10:29:32 -0700

    Update generateMig.py

--- ML/jobArray/generateMig.py
+++ ML/jobArray/generateMig.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# generateMig.py 0.0001 0.01 1 5 100
+# generateMig.py 0.0001 0.01 1 5 100 mig_val.txt
 import random
 from scipy.stats import loguniform
 import numpy as np
@@ -12,7 +12,7 @@ upper = float(sys.argv[2])
 asym = int(sys.argv[3])
 seed = int(sys.argv[4])
 n = int(sys.argv[5])
-# out = str(sys.argv[6])
+outfile = str(sys.argv[6])
 
 # upper = 0.01
 # lower = 0.0001
@@ -22,7 +22,7 @@ n = int(sys.argv[5])
 np.random.seed(seed=seed)
 random.seed(seed)
 
-file = open("mig_val.txt", "w")
+file = open(outfile, "w")
 
 if(asym == 0):
     for i in range(0,n): # all equal
